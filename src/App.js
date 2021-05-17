edf3a3e63b0d7dca3661a02c70fb62d94e4eb45b
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-05-17 20:43:49 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends React.Component {
       shows: false,
       count: 0,
     }
-    this.showHide = this.showHide.bind(this)
   }
 
   tick = () =>
@@ -24,42 +23,41 @@ class App extends React.Component {
       count: this.state.count + 1,
     })
 
-  componentDidMount = () => (this.timer = setInterval(() => this.tick(), 1000))
-
-  showHide() {
-    this.setState((currentState) => ({ shows: !currentState.shows }))
-    // console.log(this)
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000)
   }
 
-  render() {
-    let seconds = this.state.count
-    function time(seconds) {
-      const Hours = `0${Math.floor(seconds / (60 * 60))}`.slice(-2)
-      const Minutes = `0${Math.floor((seconds / 60) % 60)}`.slice(-2)
-      const Seconds = `0${Math.floor(seconds % 60)}`.slice(-2)
-      return `${Hours}:${Minutes}:${Seconds}`
-    }
+  time = () => {
+    const Hours = `0${Math.floor(this.state.count / (60 * 60))}`.slice(-2)
+    const Minutes = `0${Math.floor((this.state.count / 60) % 60)}`.slice(-2)
+    const Seconds = `0${this.state.count % 60}`.slice(-2)
+    return `${Hours}:${Minutes}:${Seconds}`
+  }
 
+  showHide = () =>
+    this.setState((currentState) => ({ shows: !currentState.shows }))
+
+  render() {
     return (
       <div className='App'>
         {/* showHide Button */}
         <button onClick={this.showHide}>
-          <h4>{this.state.shows ? 'Hide' : 'Show'} Profile</h4>
+          <h3>{this.state.shows ? 'Hide' : 'Show'} Profile</h3>
         </button>
         {/* Profile content */}
         {this.state.shows ? (
           <div className='profile'>
             <img src={this.state.person.imgSrc} alt='' />,
             <div className='txt'>
-              <h3>Name: {this.state.person.fullName}</h3>
-              <h3>Profession: {this.state.person.profession}</h3>
+              <h4>Name: {this.state.person.fullName}</h4>
+              <h4>Profession: {this.state.person.profession}</h4>
               <h4>Biography: </h4> <h5>{this.state.person.bio}</h5>
             </div>
           </div>
         ) : null}
         {/* Timer */}
         <div className='timer'>
-          <h2>You've been here for {time(seconds)}</h2>
+          <h2>You've been here for {this.time()}</h2>
         </div>
       </div>
     )
